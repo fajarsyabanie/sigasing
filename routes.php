0d1464434480def779fe5dcf2f58d10a75f72ac0
fajarsyabanie <fajarsyabanie14@gmail.com>
2022-02-05 22:25:29 +0800

    Pemecahan starter.html

---
+++ routes.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_GET['page'])){
+    $page = $_GET['page'];
+    switch ($page){
+        case '';
+        case 'home':
+            file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/040.php";
+            break;
+        default:
+            include "pages/404.php";
+    }
+}else{
+    include "pages/home.php";
+}
+
+
+
+
+?>
