0168aa11ce507116f183dacacf362104acb55986
fajarsyabanie <fajarsyabanie14@gmail.com>
2022-02-06 15:12:53 +0800

    Koneksi database dan datatables

---
+++ partials/cssdatatables.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="plugins/databatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="plugins/databatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="plugins/databatables-buttons/css/buttons.bootstrap4.min.css">
