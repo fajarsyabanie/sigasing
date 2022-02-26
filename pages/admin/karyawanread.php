63c438c5d8a302092428ad0af79050f8351157b0
fajarsyabanie <fajarsyabanie14@gmail.com>
2022-02-26 16:34:04 +0800

    Membuat halaman riwayat bagian

--- pages/admin/karyawanread.php
+++ pages/admin/karyawanread.php
@@ -55,6 +55,7 @@
                 <thead>
                     <tr>
                         <th>No</th>
+                        <th>NIK</th>
                         <th>Nama Karyawan</th>
                         <th>Bagian</th>
                         <th>Jabatan</th>
@@ -63,7 +64,7 @@
                 </thead>
                 <tfoot>
                     <tr>
-                        <th>No</th>
+                        <th>NIK</th>
                         <th>Nama Karyawan</th>
                         <th>Bagian</th>
                         <th>Jabatan</th>
@@ -94,10 +95,15 @@
                     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                     ?>
                         <tr>
-                            <td><?php echo $no++ ?></td>
                             <td><?php echo $row['nik'] ?></td>
                             <td><?php echo $row['nama_lengkap'] ?></td>
-                            <td><?php echo $row['bagian_terkini'] ?></td>
+                            <td>
+                                <?php
+                                $bagian_terkini = $row['bagian_terkini'] == "" ? "Belum Ada" : $row['bagian_terkini'];
+                                ?>
+                                <a href="?page=karyawanbagian&id=<?php echo $row['id'] ?>" class="btn bg-fuchsia btn-sm mr-1">
+                            <i class="fa fa-building"></i><?php echo $bagian_terkini?></a>
+                                </td>
                             <td><?php echo $row['jabatan_terkini'] ?></td>
                             <td>
                                 <a href="?page=karyawanupdate&id=<?php echo $row['id'] ?>" class="btn btn-primary btn-sm mr-1">
